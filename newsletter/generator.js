3b8865ce4f380ab93344de96c5634aa747665720
ted <ted.kim@tosslab.com>
2018-12-20 16:46:46 +0900

    update(): json 기준 템플릿 생성하는 코드 추가

--- newsletter/generator.js
+++ newsletter/generator.js
@@ -1,89 +1,335 @@
 
-
 /* 
 	JANDI newsletter composer
 
 */
 
 
+var contentsObject = new Object(); // 섹션과 컨텐트 정보를 섹션별로 묶어둘 객체
 
-var keyObject = new Object();
-
-var sectionObj = {};
-var contentObj = {};
-
-
+var sectionObj = {}; //섹션 객체
+var contentObj = {}; //컨텐트 객체
+var sectionLength; //섹션 객체의 길이
 
 var filepath = "http://localhost:8080/newsletter/json/JANDI Newsletter Content Template.json";
 
+
+// content-title 이 ""인 경우 두번째 줄을 생성하지 않음.
+
+var gridContentTemplate = '\
+<table class="content-template" content-id="" width="" border="0" cellpadding="0" cellspacing="0">\
+	<tr>\
+		<td>\
+			<a class="content-link" href="" target="_blank">\
+				<img class="content-thumbnail" src="" width="100%">\
+			</a>\
+		</td>\
+	</tr>\
+	<tr>\
+		<td style="padding: 1em 0 0px 0; background-color:#ffffff;">\
+			<a class="content-link" href="" target="_blank" style="text-decoration:none;font-size: 1.17em;font-weight: bold;">\
+				<span class="content-title" style=" color:#00aaea"></span>\
+			</a>\
+			<p class="content-description" style="font-size:13px;color:#777777"></p>\
+			<p class="content-author" style="color: #aaaaaa; margin-bottom: 0;"></p>\
+		</td>\
+	</tr>\
+</table>\
+';
+
+
+var listContentTemplate = '\
+<table class="content-template" content-id="" width="" border="0" cellpadding="0" cellspacing="0" >\
+	<tr>\
+		<td width="240" valign="top" rowspan="4">\
+	        <a class="content-link" href="" target="_blank">\
+				<img class="content-thumbnail" src="" width="100%">\
+	        </a>\
+		</td>\
+	</tr>\
+	<tr>\
+		<td style="padding: 0 0 0 1em;" valign="top">\
+			<a class="content-link" href="" target="_blank" style="text-decoration:none;">\
+				<span class="content-title" style=" color:#00aaea;font-size: 16px;font-weight: bold;"></span>\
+			</a>\
+		</td>\
+	</tr>\
+	<tr>\
+		<td style="padding: 0 0 0 1em;" valign="top">\
+			<p class="content-description" style="font-size:13px;color:#777777; margin-top: 0.5em; margin-bottom: 0;"></p>\
+		</td>\
+	</tr>\
+	<tr>\
+		<td  style="padding: 0 0 0 1em;" valign="bottom">\
+			<p class="content-author" style="color: #aaaaaa; ; margin: 0;"></p>\
+		</td>\
+	</tr>\
+</table>\
+';
+
+
+var sectionTemplate = '\
+<tr class="section-template" section-id="" section-order="" section-layout="">\
+	<td bgcolor="#ffffff" style="padding: 48px 0 0;">\
+    	<table border="0" cellpadding="0" cellspacing="0" width="600">\
+    		<tr>\
+            	<th bgcolor="#ebebeb" style="padding: 6px 12px 5px; text-align: left;">\
+                	<strong class="section-title" style="font-size: 20px;color: #000;"></strong>\
+				</th>\
+				<th width="20" bgcolor="#e2e2e2" >\
+				</th>\
+			</tr>\
+		</table>\
+		<table class="layout-template"></table>\
+	</td>\
+</tr>\
+';
+
+
+var col1LayoutTemplate = '\
+<table class="layout-template" border="0" cellpadding="0" cellspacing="0" width="600">\
+    <tr class="repeatable-row">\
+        <td class="repeatable-col" width="600" style="padding: 20px 0 0 0; background-color:#ffffff;">\
+            <table class="content-template"></table>\
+        </td>\
+    </tr>\
+</table>\
+';
+
+
+var col2LayoutTemplate = '\
+<table class="layout-template" border="0" cellpadding="0" cellspacing="0" width="600">\
+    <tr class="repeatable-row">\
+        <td class="repeatable-col" width="300" style="padding: 20px 5px 0 0; background-color:#ffffff;">\
+            <table class="content-template"></table>\
+        </td>\
+        <td class="repeatable-col" width="300" style="padding: 20px 0 0 5px; background-color:#ffffff;">\
+            <table class="content-template"></table>\
+        </td>\
+    </tr>\
+</table>\
+';
+
+
+/* listLayout template 은 col-1-grid와 동일         <td class="col-divider" width="10"></td>\
+var listLayoutTemplate = '\
+<table class="layout-template" border="0" cellpadding="0" cellspacing="0" width="600">\
+    <tr class="repeatable-row">\
+        <td width="600" style="padding: 20px 0 0 0; background-color:#ffffff;">\
+            <table class="content-template"></table>\
+        </td>\
+    </tr>\
+</table>\
+';
+*/
+
+
 function getJSON(filepath, callback){
+  
   $.getJSON(filepath, function(parsedData) {
+    
     sectionObj = parsedData["section"];
+    sectionLength = sectionObj.length;
     contentObj = parsedData["contents"];
     
-    callback(sectionObj,contentObj);
+    for (var i = 0; i < sectionObj.length; i++) {
+	  contentsObject[i] = $.makeArray(sectionObj[i]);
+	}
+	
+	for (var i = 0; i < contentObj.length; i++) { //length is 6
+	  
+	  if(contentObj[i].targetSectionID == 0) {
+	    contentsObject[0].push(contentObj[i]);
+	  }
+	  else if(contentObj[i].targetSectionID == 1) {
+	    contentsObject[1].push(contentObj[i]);
+	  }
+	  else if(contentObj[i].targetSectionID == 2) {
+	    contentsObject[2].push(contentObj[i]);
+	  }
+	  else if(contentObj[i].targetSectionID == 3) {
+	    contentsObject[3].push(contentObj[i]);
+	  }
+	  
+	}
+
+    callback(contentsObject);
+    
   })
   .fail(function() { console.log("error") });
 };
 
-function generateTemplate(sectionObj, contentObj){
-  console.log(sectionObj, contentObj);
-  $("td[section-id=0] .section-title").text(sectionObj[0].sectionTitle);
-  $("td[section-id=1] .section-title").text(sectionObj[1].sectionTitle);
-  $("td[section-id=2] .section-title").text(sectionObj[2].sectionTitle);
-  $("td[section-id=3] .section-title").text(sectionObj[3].sectionTitle);
-  
-  $("table[content-id=11] .contents-link").attr('href', contentObj[0].contentLink);
-  $("table[content-id=11] .contents-thumbnail").attr('src', contentObj[0].contentThumbnail);
-  $("table[content-id=11] .contents-title").html(contentObj[0].contentTitle.replace(/\n/g, "<br />"));
-  $("table[content-id=11] .contents-description").html(contentObj[0].contentDescription);
-  $("table[content-id=11] .contents-author").html(contentObj[0].contentAuthor);
-  
-  $("table[content-id=12] .contents-link").attr('href', contentObj[1].contentLink);
-  $("table[content-id=12] .contents-thumbnail").attr('src', contentObj[1].contentThumbnail);
-  $("table[content-id=12] .contents-title").html(contentObj[1].contentTitle.replace(/\n/g, "<br />"));
-  $("table[content-id=12] .contents-description").html(contentObj[1].contentDescription);
-  $("table[content-id=12] .contents-author").html(contentObj[1].contentAuthor);
-  
-  $("table[content-id=13] .contents-link").attr('href', contentObj[2].contentLink);
-  $("table[content-id=13] .contents-thumbnail").attr('src', contentObj[2].contentThumbnail);
-  $("table[content-id=13] .contents-title").html(contentObj[2].contentTitle.replace(/\n/g, "<br />"));
-  $("table[content-id=13] .contents-description").html(contentObj[2].contentDescription);
-  $("table[content-id=13] .contents-author").html(contentObj[2].contentAuthor);
-  
-  $("table[content-id=14] .contents-link").attr('href', contentObj[3].contentLink);
-  $("table[content-id=14] .contents-thumbnail").attr('src', contentObj[3].contentThumbnail);
-  $("table[content-id=14] .contents-title").html(contentObj[3].contentTitle.replace(/\n/g, "<br />"));
-  $("table[content-id=14] .contents-description").html(contentObj[3].contentDescription);
-  $("table[content-id=14] .contents-author").html(contentObj[3].contentAuthor);
-  
-  $("table[content-id=15] .contents-link").attr('href', contentObj[4].contentLink);
-  $("table[content-id=15] .contents-thumbnail").attr('src', contentObj[4].contentThumbnail);
-  $("table[content-id=15] .contents-title").html(contentObj[4].contentTitle.replace(/\n/g, "<br />"));
-  $("table[content-id=15] .contents-description").html(contentObj[4].contentDescription);
-  $("table[content-id=15] .contents-author").html(contentObj[4].contentAuthor);
 
-  $("table[content-id=16] .contents-link").attr('href', contentObj[5].contentLink);
-  $("table[content-id=16] .contents-thumbnail").attr('src', contentObj[5].contentThumbnail);
-
+function generateTemplate(contentsObject){
+  
+  console.log(contentsObject);
+  
+  $('body').append('<p class="raw-txt"></p>')
+  .find('.raw-txt').text(JSON.stringify(contentsObject, null, "\t")).attr('style','white-space:pre-wrap');
+  
+  generateSection(contentsObject);
+  
+  generateLayout(contentsObject);
+  
+  generateContent(contentsObject, 0);
+  generateContent(contentsObject, 1);
+  generateContent(contentsObject, 2);
+  generateContent(contentsObject, 3);
+ 
 }
+
 
 getJSON(filepath, generateTemplate);
 
 
 
-/*
+/* 
+ * generateTemplate 함수에서 generateSectionNLayout, generateContent 함수를 각각 실행 (반복 여부는 각 섹션 배열의 길이에 따라 다르도록..)
+ *
+ * 필요한 section entity : sectionId, sectionOrder, sectionLayout, sectionTitle, 
+ * sectionLayout 값이 col-1-grid이거나 list 인 경우 col1LayoutTemplate 치환 (그외는 col2LayoutTemplate 치환)
+ * 필요한 content entity : contentID, contentLink, contentThumbnail, contentTitle, contentDescription, contentAuthor
+ *
+ * contentCount(섹션 객체외에 컨텐트 객체를 몇개나 가지고 있는지)는 함수 내에서 처리
+ * 
+ */
+
+
+
+function generateSection(contentsObject) {
+  
+  //nth-child의 경우 모든 tr을 카운트하므로 header section tr을 포함. 따라서 2부터 카운트
+  for(var i = 0;i < sectionLength;i++) {
+	
+	var childrenCount = i+2;
+	//console.log(i, childrenCount);
+	
+	if (childrenCount < 5) {
+      $('.section-template:nth-child('+childrenCount+')')
+      .replaceWith(sectionTemplate).clone()
+      .insertAfter('.section-template:nth-child('+childrenCount+')').replaceWith(sectionTemplate);
+    }
     
-    for (var i = 0; i < keyObject.length; i++) {
-	  sectionObj[keyObject[i].KEY] = keyObject[i].KO;
-	  ENL10nObj[keyObject[i].KEY] = keyObject[i].EN;
-	  JAL10nObj[keyObject[i].KEY] = keyObject[i].JA;
-	  TCL10nObj[keyObject[i].KEY] = keyObject[i]["TC(zh-tw)"];
-	  SCL10nObj[keyObject[i].KEY] = keyObject[i]["SC(zh-cn)"];
-	}
+    $('.section-template:nth-child('+childrenCount+')')
+    .attr('section-id', contentsObject[i][0].sectionID)
+    .attr('section-order', contentsObject[i][0].sectionOrder)
+    .attr('section-layout', contentsObject[i][0].sectionLayout)
+    .find('.section-title').text(contentsObject[i][0].sectionTitle);
     
-*/
-   
+  }
+  
+}
+
+
+function generateLayout(contentsObject) {
+  
+  for(var i = 0;i < sectionLength;i++) {
+    
+    var contentLength = contentsObject[i].length -1;
+    //console.log('contentLength', contentLength, contentsObject[i][0].sectionLayout);
+
+    
+    if(contentsObject[i][0].sectionLayout=="col-1-grid" || contentsObject[i][0].sectionLayout=="list") {
+      $('.section-template[section-id="'+i+'"]').find('.layout-template').replaceWith(col1LayoutTemplate);
+      var targetID = contentsObject[i][0].sectionID;
+      
+      //list, col-1-grid layout의 경우 contentLength가 2 이상일 경우 해당 값 만큼 row를 복제한다.
+      if (contentLength > 1) {
+	  	
+        for( var cloneCount = 1; cloneCount < contentLength; cloneCount++ ) {
+
+	      // console.log(i, targetID, contentLength, cloneCount);
+	      $('.section-template[section-id="'+targetID+'"]').find('.repeatable-row:nth-child('+cloneCount+')').clone()
+	      .insertAfter('.section-template[section-id="'+targetID+'"] .repeatable-row:nth-child('+cloneCount+')');
+        }
+      }
+      
+    } 
+    else {
+	  
+	  $('.section-template[section-id="'+i+'"]').find('.layout-template').replaceWith(col2LayoutTemplate);
+	  
+	  if (contentLength > 1 && contentsObject[i][0].sectionLayout=="col-2-grid") {
+	    
+	  }
+	  
+    }
+  }
+  
+}
+
+
+var $targetContent;
+var $targetParentContent;
+
+function generateContent(contentObject, targetNum) {
+  
+  var targetObject = contentObject[targetNum];
+  //console.log(targetObject, targetNum);
+  
+  var contentLength = targetObject.length -1;
+  //console.log(contentLength);
+  
+  $targetContent = $('.section-template[section-id="'+targetNum+'"] .content-template');
+  $targetParentContent = $('.section-template[section-id="'+targetNum+'"] .layout-template');
+  
+  for(var i = 0;i < contentLength;i++) {
+    var childOrder = i+1; //0, 1, 2, 3 ,...
+    var contentWidth; //600 or 295
+    var finderPrefix; //list layout인 경우 repeatable-row > repeatable-col 형태로 보정
+    
+    //console.log(targetObject[0].sectionLayout);
+    
+    if ( targetObject[0].sectionLayout == "list") {
+      $targetContent.replaceWith(listContentTemplate);
+      contentWidth = 600;
+      finderPrefix = ".repeatable-row:nth-child("+childOrder+") .repeatable-col:nth-child(1)";
+      
+    }
+    else if (targetObject[0].sectionLayout == "col-1-grid") {
+	  $targetContent.replaceWith(gridContentTemplate);
+	  contentWidth = 600;
+	  finderPrefix = ".repeatable-row:nth-child("+childOrder+") .repeatable-col:nth-child(1)";
+    }
+    else if (targetObject[0].sectionLayout == "col-2-grid") {
+	  $targetContent.replaceWith(gridContentTemplate);
+	  contentWidth = 295;
+	  finderPrefix = ".repeatable-col:nth-child("+childOrder+")";
+    }
+    
+    // content-title 이 ""인 경우 두번째 줄 지움
+    if( targetObject[childOrder].contentTitle == "" ) {
+	  $($targetContent.selector).find('tr:nth-child(2)').remove();
+    }
+    
+    //console.log(finderPrefix);
+    
+    $($targetParentContent.selector).find(finderPrefix)
+    .find(".content-template").attr('content-id', targetObject[childOrder].contentID).attr('width', contentWidth);
+    
+    $($targetParentContent.selector).find(finderPrefix)
+    .find(".content-link").attr('href', targetObject[childOrder].contentLink);
+    
+    $($targetParentContent.selector).find(finderPrefix)
+    .find(".content-thumbnail").attr('src', targetObject[childOrder].contentThumbnail);
+    
+    $($targetParentContent.selector).find(finderPrefix)
+    .find(".content-title").html(targetObject[childOrder].contentTitle.replace(/\n/g, "<br />"));
+    
+    $($targetParentContent.selector).find(finderPrefix)
+    .find(".content-description").html(targetObject[childOrder].contentDescription);
+    
+    $($targetParentContent.selector).find(finderPrefix)
+    .find(".content-author").html(targetObject[childOrder].contentAuthor);
+
+
+  }
+  
+}
+
+  
+  
+
    
    
    
