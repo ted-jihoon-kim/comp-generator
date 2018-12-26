bb63db66d57b79db8bb945012f5ec9fa0b159c70
ted <ted.kim@tosslab.com>
2018-12-26 11:51:24 +0900

    update(): dummy json 경로 수정 & 인덱스 tbody 엘리먼트 추가 & export시 파일명 변경

--- newsletter/downloader.js
+++ newsletter/downloader.js
@@ -5,20 +5,25 @@
 */
 
 var htmlPreText = "<!DOCTYPE HTML PUBLIC '-//W3C//DTD HTML 4.01//EN' 'http://www.w3.org/TR/html4/strict.dtd'>";
+var titleTemplate = "JANDI Newsletter ";
 var convertedString;
+
 
 //download current template.
 $('#downloadTemplate').click(function() {
-
+	
+	//페이지 타이틀 보정
+	titleTemplate = titleTemplate + currentMonth + "_"+ currentYear;
+	$('title').text(titleTemplate);
 	
 	convertedString = $('html').html();
 	
 	convertedString = convertedString.replace(/<script.*<\/script>/g, '') //스크립트 영역 제거
 					  .replace(/<!--.*-->/g, '') //주석 제거
-					  .replace(/<button.*<\/button>/g, '') 
-					  .replace(/<\/body>/, '</body>\n</html>')
+					  .replace(/<div.*<\/div>/g, '') //하단 버튼 영역 제거
+					  .replace(/<\/body>/, '</body>\n</html>') //html 닫는 태그 추가
 					  
-					  .replace(/<head>/, htmlPreText +"\n<html>\n<head>").replace(/\n/g, "\n"); //줄바꿈 적용
+					  .replace(/<head>/, htmlPreText +"\n<html>\n<head>").replace(/\n/g, "\n"); //doctype, head 태그 추가
 					  
 					 
 	
@@ -26,7 +31,7 @@ $('#downloadTemplate').click(function() {
 	//download(data, strFileName, strMimeType);
 	
 	console.log(convertedString)
-	download(convertedString, 'JANDI_newsletter.html', "text/html");
+	download(convertedString, titleTemplate+'.html', "text/html");
 	
 	
 });
